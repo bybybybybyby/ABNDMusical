2fac85ab76f25010b65f4be22bf1fa06265ea48e
brian <bybyby@gmail.com>
2018-05-23 01:36:36 -0700

    Made listView clickable.  Able to get artist and song for clicked item to show on toast.

--- app/src/main/java/com/example/android/abndmusical/ArtistsActivity.java
+++ app/src/main/java/com/example/android/abndmusical/ArtistsActivity.java
@@ -1,8 +1,14 @@
 package com.example.android.abndmusical;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 
@@ -17,7 +23,7 @@ public class ArtistsActivity extends AppCompatActivity{
         ArrayList<Song> songs = new ArrayList<Song>();
         songs.add(new Song("Beethoven", "Symphony"));
         songs.add(new Song("Michael Jackson", "Bad"));
-
+        songs.add(new Song("Miley Cyrus", "Wrecking Ball"));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
@@ -26,11 +32,25 @@ public class ArtistsActivity extends AppCompatActivity{
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // word_list.xml file.
-        ListView listView = (ListView) findViewById(R.id.list);
+        final ListView listView = (ListView) findViewById(R.id.list);
 
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
+        // Make the {@link ListView} use the {@link SongAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Song} in the list.
         listView.setAdapter(adapter);
 
+
+        listView.setClickable(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Song currentSong = (Song)listView.getItemAtPosition(position);
+//                String str=(String)o;//As you are using Default String Adapter
+                Toast.makeText(getApplicationContext(),currentSong.getSongArtist(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),currentSong.getSongTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
+
 }
--- app/src/main/java/com/example/android/abndmusical/NowPlayingActivity.java
+++ app/src/main/java/com/example/android/abndmusical/NowPlayingActivity.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 
-public class NowPlayingActivity extends AppCompatActivity{
+public class NowPlayingActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_now_playing);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing);
 
 
-        }
+    }
 }
